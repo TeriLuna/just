c0566d3577b509e44e9367d000ee0840c8c073de
TeriLuna <catherinelozada7@gmail.com>
2022-07-10 13:01:29 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./assets/styles/App.css";
+
+export const App = () => {
+  return (
+    <>
+      Acá irá el componente de header, el hero section, y los demás componentes
+      de la landinpage.
+      <br />
+      En la carpeta componentes puedes crear la carpeta que requieras para
+      ubicar cada componente
+      <br />
+      Cualquier duda me cuentas.
+      <br />
+      La idea es que todo trabajo que hagas lo montes en github para yo poderlo
+      descargar y viceversa. para que ambos mantengamos el proyecto actualizado.
+    </>
+  );
+};
