db0725cfd5fb64d318ce0e802fcb97014b32896d
TeriLuna <catherinelozada7@gmail.com>
2022-07-13 18:10:18 -0500

    Adding hero img to landing page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+// import bgImg from "../../assets/images/bg/bg-hero-home.jpg";
 
 export const Home = () => {
   return (
     <section>
-      <div className="container px-4">
+      <div className="container">
         <div className="flex-row">
           <div className=" col-span-12">
             <div
-              className="bg-img hero-img"
-              style={{ backgroundImage: "url(/imgages/bg/bg-hero-home.jpg)" }}
+              className="fullscreen bg-img hero-img flex items-center justify-center absolute -z-10"
             ></div>
             <span className="tpurple">Cambia tu vida con</span>
             <span>Bienestar para ti y los demás</span>
