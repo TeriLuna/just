e9c0a17a267e4c9898c7306ff0126b04c6da9763
TeriLuna <catherinelozada7@gmail.com>
2022-07-18 17:47:22 -0500

    hero section with button and letters

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,28 @@
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-// import bgImg from "../../assets/images/bg/bg-hero-home.jpg";
 
 export const Home = () => {
   return (
     <section>
-      <div className="container">
-        <div className="flex-row">
-          <div className=" col-span-12">
-            <div className="fullscreen bg-img hero-img flex items-center justify-center absolute -z-10"></div>
-            <div className="">
-              <span className="tpurple">Cambia tu vida con</span>
-              <span>Bienestar para ti y los demás</span>
+      <div className=" max-w-md mx-auto md:max-w-2xl">
+        <div className=" md:flex">
+          <div className="md:shrink-0">
+            <div className="fullscreen left-0 bg-no-repeat bg-cover bg-center bg-img hero-img absolute"></div>
+            <div className="max-w-md mx-auto">
+              <div className="absolute flex flex-col w-1/3 top-72 left-64 sm:flex sm:top-0 sm:left-0 md:top-44 md:left-60">
+                <span className="tpurple font-normal text-5xl sm:tpurple-light">
+                  Cambia tu vida con
+                </span>
+                <span className=" text-6xl font-normal text-white mt-3 sm:text-3xl">
+                  Bienestar para ti y los demás
+                </span>
+                <button className="bg-purple rounded-lg py-5 px-2 w-96 mt-10 text-4xl">
+                  Contáctanos
+                </button>
+                <span className="text-white mt-40 text-5xl">
+                  Obtén ingresos extra como consultor/a Just ¡AHORA!
+                </span>
+              </div>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./public/index.html",
@@ -6,7 +8,11 @@ module.exports = {
     "./components/**/*.{html, js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
