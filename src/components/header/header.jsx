fef7c93efbc01855320794882629dfdbcd4196ad
TeriLuna <catherinelozada7@gmail.com>
2022-07-23 12:07:14 -0500

    Navbar modifications: position, width, z-index

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,25 +1,75 @@
-import React from 'react'
-import {Link} from 'react-scroll'
+import React from "react";
+import { Link } from "react-scroll";
 
 export const Header = () => {
-    return (
-        <nav className='navJust'>
-            <div className='contIzq'>
-                <div className="imgHeader">
-                    <img  src={require("../../assets/images/logo/just.png")} alt="logo1" />
-                </div>
-                <div className="imgHeader">
-                    <img  src={require("../../assets/images/logo/logo-bienestar.png")} alt="logo2" />
-                </div>
-                
-            </div>
-            <div className='contDer'>
-            <Link  to='home' spy={true} smooth={true} offset={-50} duration={500} className="tpurple text-xl bold">Home</Link>
-            <Link to='services' spy={true} smooth={true} offset={-50} duration={500} className="tpurple text-xl ">¿Qué es JUST?</Link>
-            <Link to='iniciacion' spy={true} smooth={true} offset={50} duration={500} className="tpurple text-xl ">¿Cómo iniciar?</Link>
-            <Link to='beneficios' spy={true} smooth={true} offset={50} duration={500} className="tpurple text-xl ">Beneficios</Link>
-            <Link to='contacto' spy={true} smooth={true} offset={50} duration={500} className="tpurple text-xl ">Contacto</Link>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav
+      className="navJust absolute w-full z-10"
+      style={{ background: "#ffffff00" }}
+    >
+      <div className="contIzq">
+        <div className="imgHeader">
+          <img src={require("../../assets/images/logo/just.png")} alt="logo1" />
+        </div>
+        <div className="imgHeader">
+          <img
+            src={require("../../assets/images/logo/logo-bienestar.png")}
+            alt="logo2"
+          />
+        </div>
+      </div>
+      <div className="contDer">
+        <Link
+          to="home"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="tpurple text-xl bold"
+        >
+          Home
+        </Link>
+        <Link
+          to="services"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="tpurple text-xl "
+        >
+          ¿Qué es JUST?
+        </Link>
+        <Link
+          to="iniciacion"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          className="tpurple text-xl "
+        >
+          ¿Cómo iniciar?
+        </Link>
+        <Link
+          to="beneficios"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          className="tpurple text-xl "
+        >
+          Beneficios
+        </Link>
+        <Link
+          to="contacto"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+          className="tpurple text-xl "
+        >
+          Contacto
+        </Link>
+      </div>
+    </nav>
+  );
+};
