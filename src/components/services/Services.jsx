2b492ffe03c0c98fd2c39507d6398c82f24dfe65
TeriLuna <catherinelozada7@gmail.com>
2022-07-25 19:17:01 -0500

    Adding services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import ListServices from "./ListServices";
 
 const Services = () => {
   return (
-    <div id="services" className="relative p-28">
+    <section id="services" className="relative p-28">
       <div className=" container">
         <div className=" row-auto">
-          <h4 className="tpurple-light">Pioneros en terapia herbal</h4>
-          <h5 className="tpurple">Nuestros productos</h5>
+          <h4
+            className="tpurple-light text-5xl font-extralight"
+            style={{ fontFamily: "AmsterdamOne" }}
+          >
+            Pioneros en terapia herbal
+          </h4>
+          <h5 className="tpurple text-4xl">Nuestros productos</h5>
+        </div>
+        <div className="row-auto mt-8">
+          <div className="col-auto">
+            <p className=" text-3xl tpurple-light">
+              Los productos Just se basan en recetas de bienestar que utilizan
+              prácticas ancestrales con hierbas medicinales para atender
+              necesidades cotidianas, con fórmulas originadas en los
+              laboratorios Just en los Alpes Suizos con potentes componentes
+              activos que brindan propiedades de bienestar y belleza
+              extraordinarias.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      <ListServices />
+    </section>
   );
 };
 
