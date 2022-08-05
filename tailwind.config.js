f3732999ed57b7b388845265b27b8b44e9f5dcd5
TeriLuna <catherinelozada7@gmail.com>
2022-08-04 19:55:52 -0500

    deleting plugins from tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss/typography"),
-    require("tailwindcss/forms"),
-    require("tailwindcss/line-clamp"),
-    require("tailwindcss/aspect-ratio"),
-  ],
+  plugins: [],
 };
